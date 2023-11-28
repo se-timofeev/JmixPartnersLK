7a84965c01d5d432c32123c2b14db8c3434ca2c7
Sergei Timofeev <se.timofeev@icloud.com>
2023-11-28 20:07:26 +0300

    Initial commit

---
+++ src/main/java/com/company/jmixstart1/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.jmixstart1.view.user;
+
+import com.company.jmixstart1.entity.User;
+import com.company.jmixstart1.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
